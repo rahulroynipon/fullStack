8deb76e10aff2ab6fea40aee1fe8edeb73dd4873
rahulroynipon <rahulroynipon@gmail.com>
2024-06-07 00:59:01 +0600

    conect to mongo atlas

---
+++ Backend/src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import connectDB from "./DB/index.js";
+
+dotenv.config({
+    path: "./env",
+});
+connectDB();
+
+// import express from "express";
+// import express from "express";
+
+// const app = express();
+
+// (async () => {
+//     try {
+//         mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error", (error) => {
+//             console.error("ERROR: ", error);
+//             throw error;
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         });
+//     } catch (error) {
+//         console.error("ERROR: ", error);
+//         throw error;
+//     }
+// })();
